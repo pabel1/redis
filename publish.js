04cbe0e9ff10e4a7403dd0fb83f16045f119735b
pabel1 <pabelahamed39@gmail.com>
2023-09-17 23:18:41 +0600

    redis publish successfull

--- publish.js
+++ publish.js
@@ -15,6 +15,21 @@ const connect = async () => {
 
 connect();
 
+app.get("/", (req, res) => {
+  console.log("Publisher is 4001 port");
+  res.send({ message: "Publisher is 4001 port " });
+});
+app.get("/publish", async (req, res) => {
+  const id = Math.floor(Math.random() * 10);
+  console.log(`Publisher id ${id}`);
+  let data = {
+    id: id,
+    message: `Publisher id is ${id} `,
+  };
+  await publisher.publish("message", JSON.stringify(data));
+  res.send({ message: "data Publish successfull!!" });
+});
+
 app.listen(4001, () => {
   console.log(`Publisher server start on posrt 4001`);
 });
